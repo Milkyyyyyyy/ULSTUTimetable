173af53e0d4c1f07048e853c5af8bb7d3209497b
Milkyyyyyyy <brosuper500@gmail.com>
2026-09-06 12:58:51 +0400

    test

--- states/states.py
+++ states/states.py
@@ -1,32 +1,7 @@
 from aiogram.fsm.state import State, StatesGroup
 
-
 class Registration(StatesGroup):
 	waiting_for_login = State()
 	waiting_for_password = State()
-	waiting_for_facult = State()
 	waiting_for_group = State()
 	waiting_for_subgroup = State()
-
-
-class MainMenu(StatesGroup):
-	main_menu = State()
-
-
-class Settings(StatesGroup):
-	settings = State()
-	waiting_for_login = State()
-	waiting_for_password = State()
-	waiting_for_group = State()
-	waiting_for_subgroup = State()
-	waiting_for_facult = State()
-	waiting_for_deletion_acceptation = State()
-
-
-class ScheduleSelection(StatesGroup):
-	selecting_day = State()
-
-
-class NotificationSettings(StatesGroup):
-	notification_setting = State()
-	wait_for_time = State()
--- validator/group.py
+++ validator/group.py
@@ -1,8 +1,16 @@
 import re
 
+
+GROUP_PATTERN = re.compile(
+    r"^[А-ЯЁа-яё]+-\d{2}$"
+)
+
+def is_group_valid(group: str) -> bool|None:
+	return GROUP_PATTERN.fullmatch(group)
+
 def normalize_group(group: str) -> str:
     group = group.strip()
     group = re.sub(r"\s*-\s*", "-", group)
     group = re.sub(r"\s+", "", group)
 
-    return group.upper()
+    return group
